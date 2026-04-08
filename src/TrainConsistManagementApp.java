1b0ba1a272585953125fa97aa32fa67c54f02909
vartikajamwal <vj2081@srmist.edu.in>
2026-04-08 14:27:01 +0530

    UC1: Display Consist Summary

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully ...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist : " + trainConsist);
+        System.out.println("System ready for operations ...");
+    }
+}
